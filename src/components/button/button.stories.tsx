9eea127ce18f852b726fca3412ada0cc113fd7b5
Serbe <serbenv@gmail.com>
2023-08-17 15:00:47 +0300

    new button

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -4,42 +4,50 @@ import { useState } from "react";
 import { Colors, ColorValues, Sizes } from "../../utils/variables";
 import { Button, IButtonProps } from "./index";
 
-// <button className="mx-2 min-w-[110px] rounded-lg border border-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:opacity-75 focus:ring focus:ring-pink-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-// Custom
-// </button>;
-
 export const AllVariants = (): JSX.Element => (
   <div className="container mx-auto">
-    <div className="flex items-end py-1">
-      <Button isDisabled>Disabled</Button>
-      <Button isDisabled isOutlined>
+    <div className="my-1 flex items-end">
+      <Button className="mx-1" isDisabled>
+        Disabled
+      </Button>
+      <Button className="mx-1" isDisabled isOutlined>
         Disabled O
       </Button>
-      <Button isUpperCase>UpperCase</Button>
-      <Button isUpperCase isOutlined>
+      <Button className="mx-1" isUpperCase>
+        UpperCase
+      </Button>
+      <Button className="mx-1" isUpperCase isOutlined>
         UpperCase O
       </Button>
     </div>
-    <div className="flex items-end py-1">
-      <Button isFullwith>FullWidth</Button>
-      <Button isFullwith isOutlined>
+    <div className="my-1 flex items-end">
+      <Button className="mx-1" isFullwith>
+        FullWidth
+      </Button>
+      <Button className="mx-1" isFullwith isOutlined>
         FullWidth O
       </Button>
     </div>
-    <div className="flex items-end py-1">
-      <Button>D</Button>
-      <Button isOutlined>OD</Button>
-      <Button size="small">S</Button>
-      <Button isOutlined size="small">
-        OS
+    <div className="my-1 flex items-end">
+      <Button className="mx-1">Default</Button>
+      <Button className="mx-1" isOutlined>
+        Default O
       </Button>
-      <Button size="large">L</Button>
-      <Button isOutlined size="large">
-        OL
+      <Button className="mx-1" size="small">
+        Small
+      </Button>
+      <Button className="mx-1" isOutlined size="small">
+        Small O
+      </Button>
+      <Button className="mx-1" size="large">
+        Large
+      </Button>
+      <Button className="mx-1" isOutlined size="large">
+        Large O
       </Button>
     </div>
     {ColorValues.map((color) => (
-      <div className="flex items-end py-1" key={`div-${color}`}>
+      <div className="my-1 flex items-end" key={`div-${color}`}>
         {[undefined, "small", "large"].map((size) =>
           [false, true].map((isOutlined) => (
             <Button
@@ -47,6 +55,7 @@ export const AllVariants = (): JSX.Element => (
               size={size as Sizes}
               isOutlined={isOutlined}
               key={`${isOutlined ? "O" : ""}${color}${size ?? ""}`}
+              className="mx-1"
             >{`${color} ${size ?? ""}${isOutlined ? " O" : ""}`}</Button>
           )),
         )}
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,14 +1,10 @@
 import type { FC, MouseEventHandler, ReactNode } from "react";
-import React from "react";
 
 import { classNames, classToString } from "../../utils/classNames";
 import {
   IClassName,
   backgroundColor,
   borderColor,
-  focusRingColor,
-  hoverShadowColor,
-  shadowColor,
   textColor,
   type Colors,
   type IPadding,
@@ -31,25 +27,25 @@ export interface IButtonProps {
 
 interface IButtonSize {
   padding: IPadding;
-  textSize: string;
-  width: string;
+  fontSize: string;
+  minWidth: string;
 }
 
 const buttonSize: Record<Sizes, IButtonSize> = {
   small: {
-    padding: { x: "px-4", y: "py-2.5" },
-    textSize: "text-xs",
-    width: "min-w-[80px]",
+    padding: { x: "px-2", bottom: "pb-1.5", top: "pt-1" },
+    fontSize: "text-xs",
+    minWidth: "min-w-[80px]",
   },
   normal: {
-    padding: { x: "px-6", y: "py-2.5" },
-    textSize: "text-sm",
-    width: "min-w-[100px]",
+    padding: { x: "px-3", bottom: "pb-1.5", top: "pt-1" },
+    fontSize: "text-sm",
+    minWidth: "min-w-[100px]",
   },
   large: {
-    padding: { x: "px-7", y: "py-3" },
-    textSize: "text-base",
-    width: "min-w-[120px]",
+    padding: { x: "px-4", bottom: "pb-1.5", top: "pt-1" },
+    fontSize: "text-base",
+    minWidth: "min-w-[120px]",
   },
 };
 
@@ -62,86 +58,44 @@ const buttonClasses = (
 ): IClassName => {
   const bSize = buttonSize[size];
   return {
-    margin: {
-      right: "mr-4",
+    size: {
+      width: isFullwith ? "w-full" : undefined,
+      minWidth: bSize.minWidth,
     },
     border: {
-      radius: "rounded-lg",
-      color: isOutlined ? borderColor[color] : undefined,
-      width: isOutlined ? "border" : undefined,
+      borderRadius: "rounded-md",
+      borderWidth: "border",
+      borderColor: borderColor[color],
     },
     background: {
-      color: isOutlined ? undefined : backgroundColor[color],
+      color: isOutlined ? "bg-white" : backgroundColor[color],
     },
     padding: {
       x: bSize.padding.x,
-      y: bSize.padding.y,
+      bottom: bSize.padding.bottom,
+      top: bSize.padding.top,
     },
-    font: {
-      family: "font-sans",
-      color: isOutlined ? textColor[color] : "text-white",
-      size: bSize.textSize,
-      weight: "font-bold",
+    typography: {
+      fontSize: bSize.fontSize,
+      fontWeight: "font-bold",
+      textColor: isOutlined ? textColor[color] : "text-white",
+      textTransform: isUpperCase ? "uppercase" : undefined,
     },
-    text: {
-      transform: isUpperCase ? "uppercase" : "capitalize",
-    },
-    width: isFullwith ? "w-full" : bSize.width,
-    box: {
-      shadow: isOutlined ? undefined : "shadow-md",
-      color: isOutlined ? undefined : shadowColor[color],
-    },
-    transition: {
-      transition: "transition-all",
+    effect: {
+      boxShadow: "shadow-md",
     },
     hover: {
-      opacity: isOutlined ? "hover:opacity-75" : undefined,
-      box: {
-        shadow: isOutlined ? undefined : "hover:shadow-lg",
-        color: isOutlined ? undefined : hoverShadowColor[color],
-      },
-    },
-    focus: {
-      opacity: isOutlined ? undefined : "focus:opacity-[0.85]",
-      ring: {
-        ring: isOutlined ? "focus:ring" : undefined,
-        color: isOutlined ? focusRingColor[color] : undefined,
-      },
-      box: {
-        shadow: isOutlined ? undefined : "focus:shadow-none",
-      },
-    },
-    active: {
-      opacity: "active:opacity-[0.85]",
-      box: {
-        shadow: isOutlined ? undefined : "active:shadow-none",
-      },
-    },
-    disabled: {
-      pointerEvent: "disabled:pointer-events-none",
-      opacity: "disabled:opacity-50",
-      box: {
-        shadow: "disabled:shadow-none",
+      effect: {
+        boxShadow: "hover:shadow-lg",
       },
     },
   };
 };
 
-// blue    className="mx-2 rounded-lg                                 bg-blue-500        py-3         px-6         font-sans text-xs  font-bold capitalize text-white    shadow-md     shadow-blue-500/20    transition-all hover:shadow-lg     hover:shadow-blue-500/40                    focus:opacity-[0.85] focus:shadow-none                                                               active:opacity-[0.85] active:shadow-none   disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-// red     className="mx-2 rounded-lg                                 bg-red-500         py-3         px-6         font-sans text-xs  font-bold capitalize text-white    shadow-md     shadow-red-500/20     transition-all hover:shadow-lg     hover:shadow-red-500/40                     focus:opacity-[0.85] focus:shadow-none                                                               active:opacity-[0.85] active:shadow-none   disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-// green   className="mx-2 rounded-lg                                 bg-green-500       py-3         px-6         font-sans text-xs  font-bold capitalize text-white    shadow-md     shadow-green-500/20   transition-all hover:shadow-lg     hover:shadow-green-500/40                   focus:opacity-[0.85] focus:shadow-none                                                               active:opacity-[0.85] active:shadow-none   disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-// orange  className="mx-2 rounded-lg                                 bg-orange-500      py-3         px-6         font-sans text-xs  font-bold capitalize text-white    shadow-md     shadow-orange-500/20  transition-all hover:shadow-lg     hover:shadow-orange-500/40                  focus:opacity-[0.85] focus:shadow-none                                                               active:opacity-[0.85] active:shadow-none   disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-// pink    className="mx-2 rounded-lg                                 bg-pink-500        py-3         px-6         font-sans text-xs  font-bold uppercase  text-white    shadow-md     shadow-pink-500/20    transition-all hover:shadow-lg     hover:shadow-pink-500/40                    focus:opacity-[0.85] focus:shadow-none                                                               active:opacity-[0.85] active:shadow-none   disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-// pink ou className="mx-2 rounded-lg border          border-pink-500                    py-3         px-6         font-sans text-xs  font-bold uppercase  text-pink-500                                     transition-all                                                hover:opacity-75                                          focus:ring        focus:ring-pink-200                       active:opacity-[0.85]                      disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-// bc                 mx-2 rounded-lg bc.border.width bc.border.color bc.backgroundColor bc.padding.y bc.padding.x font-sans textSize font-bold capitalize bc.textColor  bc.shadow.box bc.shadow.color       transition-all bc.hover.shadow.box bc.hover.shadow.box        bc.hover.opacity bc.focus.opacity     bc.focus.shadow.box bc.focus.ring.box bc.focus.ring.color                       active:opacity-[0.85] bc.active.shadow.box disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none
-
-// pink gr className="mx-2 rounded-lg                        bg-gradient-to-tr from-pink-600 to-pink-400 py-3 px-6 font-sans text-xs  font-bold uppercase  text-white    shadow-md shadow-pink-500/20   transition-all hover:shadow-lg hover:shadow-pink-500/40                                                                                                                    active:opacity-[0.85]                    disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-// pink z  className="mx-2 rounded-lg                                                                    py-3 px-6 font-sans text-xs  font-bold uppercase  text-pink-500                                transition-all                                            hover:bg-pink-500/10                                                                       active:bg-pink-500/30                                          disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-
 export const Button: FC<IButtonProps> = ({
   children,
   className,
-  color = "blue",
+  color = "slate",
   isDisabled,
   isFullwith = false,
   isOutlined = false,
