9751a0bf3ba1627ec9ee49857310a5904bae0f1b
Serbe <serbenv@gmail.com>
2023-10-11 16:36:38 +0300

    fix positions

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -52,37 +52,51 @@ interface IInputSize {
   fontSize: string;
   height: string;
   leading: string;
-  minWidth: string;
 }
 
 interface IElementSize {
   divHeight: string;
-  divWidth: string;
   iconHeight: string;
   iconWidth: string;
   paddingRight: string;
 }
-
-const minW100 = "min-w-[100px]";
 
 const getInputSize: Record<Sizes, IInputSize> = {
   large: {
     fontSize: "text-lg",
     height: "h-9",
     leading: "leading-7",
-    minWidth: minW100,
   },
   normal: {
     fontSize: "text-base",
     height: "h-8",
     leading: "leading-tight",
-    minWidth: minW100,
   },
   small: {
     fontSize: "text-sm",
     height: "h-7",
     leading: "leading-3",
-    minWidth: minW100,
+  },
+};
+
+const getElementSize: Record<Sizes, IElementSize> = {
+  large: {
+    divHeight: "min-h-[3.3rem]",
+    iconHeight: "h-8",
+    iconWidth: "w-8",
+    paddingRight: "pr-9",
+  },
+  normal: {
+    divHeight: "min-h-[3.1rem]",
+    iconHeight: "h-7",
+    iconWidth: "w-7",
+    paddingRight: "pr-8",
+  },
+  small: {
+    divHeight: "min-h-[2.9rem]",
+    iconHeight: "h-6",
+    iconWidth: "w-6",
+    paddingRight: "pr-7",
   },
 };
 
@@ -110,9 +124,7 @@ const getInputClass = (
     focus: {
       border: {
         borderColor: isOutlined ? undefined : focusBorderColor[color],
-        // borderWidth: isOutlined ? undefined : "focus:border-b-1",
         outlineColor: isOutlined ? focusOutlineColor[color] : undefined,
-        // outlineWidth: isOutlined ? "focus:outline-1" : undefined,
       },
     },
     hover: {
@@ -120,20 +132,19 @@ const getInputClass = (
         borderColor: isOutlined ? undefined : hoverBorderColor[color],
         outlineColor: isOutlined ? hoverOutlineColor[color] : undefined,
       },
-      // effect: {
-      //   boxShadow: "hover:shadow",
-      // },
     },
     interactivity: {
       appearance: "appearance-none",
     },
     layout: {
-      left: "left-1",
       position: "relative",
       top: "top-3",
     },
+    margin: {
+      x: isOutlined ? "mx-1" : undefined,
+    },
     padding: {
-      left: isOutlined ? "pl-1" : undefined,
+      left: isOutlined ? "pl-1" : "undefined",
       right: paddingRight ?? `${isOutlined ? "pr-1" : undefined}`,
       y: "py-1",
     },
@@ -144,7 +155,6 @@ const getInputClass = (
     },
     size: {
       height: inputSize.height,
-      minWidth: inputSize.minWidth,
       width: isFullwidth ? "w-full" : undefined,
     },
     typography: {
@@ -152,30 +162,6 @@ const getInputClass = (
       lineHeight: inputSize.leading,
     },
   };
-};
-
-const getElementSize: Record<Sizes, IElementSize> = {
-  large: {
-    divHeight: "min-h-[3.3rem]",
-    divWidth: "min-w-[16.35rem]",
-    iconHeight: "h-8",
-    iconWidth: "w-8",
-    paddingRight: "pr-10",
-  },
-  normal: {
-    divHeight: "min-h-[3.1rem]",
-    divWidth: "min-w-[14.6rem]",
-    iconHeight: "h-7",
-    iconWidth: "w-7",
-    paddingRight: "pr-9",
-  },
-  small: {
-    divHeight: "min-h-[2.9rem]",
-    divWidth: "min-w-[13.5rem]",
-    iconHeight: "h-6",
-    iconWidth: "w-6",
-    paddingRight: "pr-8",
-  },
 };
 
 export const Input: FC<IInputProperties> = ({
@@ -226,15 +212,18 @@ export const Input: FC<IInputProperties> = ({
   );
 
   const inputClassNames = classNames(classToString(ic), className);
+  const iconClassName = classNames(
+    "absolute",
+    isOutlined ? "right-1" : "right-0",
+    "top-3.5",
+    "grid",
+    elementSize.iconHeight,
+    elementSize.iconWidth,
+    "place-items-center",
+  );
 
   const Icon = (): JSX.Element | null =>
-    icon ? (
-      <div
-        className={`absolute right-1.5 top-3.5 grid ${elementSize.iconHeight} ${elementSize.iconWidth} place-items-center`}
-      >
-        {icon}
-      </div>
-    ) : null;
+    icon ? <div className={iconClassName}>{icon}</div> : null;
 
   const Label = (): JSX.Element | null =>
     label ? (
@@ -253,7 +242,6 @@ export const Input: FC<IInputProperties> = ({
     "relative",
     isFullwidth ? "w-full" : undefined,
     elementSize.divHeight,
-    elementSize.divWidth,
     backgroundColor[bgColor],
   );
 
